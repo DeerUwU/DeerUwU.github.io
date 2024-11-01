ae49c0f703b09aff7a1b254c7902f5f4e7a1b677
Deer ♡ <61318829+DeerUwU@users.noreply.github.com>
2024-11-01 17:51:52 +0100

    finally fixed doubleclick lmao

--- desktop/index.service.worker.js
+++ desktop/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1730255305|75115846';
+const CACHE_VERSION = '1730479863|546774161';
 /** @type {string} */
 const CACHE_PREFIX = 'Deer's Desktop-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
